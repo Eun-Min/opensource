96f6087aba9dd5a67180734359e59fcc206e1521
waristo <wildstrip@naver.com>
2016-10-16 21:23:02 +0900

    rock-scissors-paper game

---
+++ rock.py
@@ -0,0 +1,77 @@
+"""
+Game
+
+ * Author : 김원배
+ * Created : 김원배
+ * Last modified : 김원배
+"""
+
+import random
+
+computer = ["가위","바위","보"]
+hand = input("가위바위보 중에 하나를 고르세요:  ")
+computerhand = random.choice(computer)
+i = 0
+computerwin = 0
+playerwin = 0
+
+
+while i<10 :
+
+	hand = input("가위바위보 중에 하나를 고르세요:  ")
+	computerhand = random.choice(computer)
+
+	if hand == "가위" :
+		if computerhand == "가위":
+			print("컴퓨터 : 가위")
+			print("무승부입니다.")
+			i += 1
+
+		elif computerhand == "바위":
+			print("컴퓨터 : 바위")
+			print("컴퓨터가 이겼습니다.")
+			i += 1
+			computerwin += 1
+		else :
+			print("컴퓨터 : 보")
+			print("플레이어가 이겼습니다.")
+			i += 1
+			playerwin += 1
+
+	elif hand == "바위" :
+		if computerhand == "가위":
+			print("컴퓨터 : 가위")
+			print("플레이어가 이겼습니다.")
+			i += 1
+			playerwin += 1
+
+		elif computerhand == "바위":
+			print("컴퓨터 : 바위")
+			print("무승부입니다.")
+			i += 1
+
+		else :
+			print("컴퓨터 : 보")
+			print("컴퓨터가 이겼습니다.")
+			i += 1
+			computerwin += 1
+
+	else:
+		if computerhand == "가위":
+			print("컴퓨터 : 가위")
+			print("컴퓨터가 이겼습니다.")
+			i += 1
+			computerwin += 1
+
+		elif computerhand == "바위":
+			print("컴퓨터 : 바위")
+			print("플레이어가 이겼습니다.")
+			i += 1
+			playerwin += 1
+
+		else :
+			print("컴퓨터 : 보")
+			print("무승부입니다.")
+			i += 1
+
+print("컴퓨터 : " ,computerwin ," 플레이어 : " ,playerwin)
