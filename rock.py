0dbf24136af5db7097cf7b905dab7474f29b2091
waristo <wildstrip@naver.com>
2016-10-25 17:25:28 +0900

    no message

--- rock.py
+++ rock.py
@@ -13,63 +13,55 @@ computerhand = random.choice(computer)
 i = 0
 computerwin = 0
 playerwin = 0
-draw = 0
 
-while True :
-	print("=======================================")		
+
+while i<10 :
+
 	hand = input("가위바위보 중에 하나를 고르세요 (종료:x)  ")
-	print("=======================================")
-	
 	computerhand = random.choice(computer)
 
 	if hand == "가위" :
 		if computerhand == "가위":
 			print("컴퓨터 : 가위")
 			print("무승부입니다.")
-			draw += 1
-
+			i++
 		elif computerhand == "바위" :
 			print("컴퓨터 : 바위")
 			print("컴퓨터가 이겼습니다.")
-			computerwin += 1
-
+			i++
 		else :
 			print("컴퓨터 : 보")
 			print("플레이어가 이겼습니다.")
 			playerwin += 1
-
+			i++
 	elif hand == "바위" :
 		if computerhand == "가위":
 			print("컴퓨터 : 가위")
 			print("플레이어가 이겼습니다.")
 			playerwin += 1
-
+			i++
 		elif computerhand == "바위":
 			print("컴퓨터 : 바위")
 			print("무승부입니다.")
-			draw += 1
-
+			i++
 		else :
 			print("컴퓨터 : 보")
 			print("컴퓨터가 이겼습니다.")
 			computerwin += 1
-
+			i++
 	elif hand == "보" :
 		if computerhand == "가위":
 			print("컴퓨터 : 가위")
 			print("컴퓨터가 이겼습니다.")
 			computerwin += 1
-
+			i++
 		elif computerhand == "바위":
 			print("컴퓨터 : 바위")
 			print("플레이어가 이겼습니다.")
 			playerwin += 1
-
+			i++
 		else :
 			print("컴퓨터 : 보")
 			print("무승부입니다.")
-			draw += 1
-
-	elif hand == "x" :
-		break
-print("컴퓨터 " ,computerwin , " : ",playerwin, " 플레이어 ( 무승부 :",draw,"번)" )
+			i++
+print("컴퓨터 : " ,computerwin ," 플레이어 : " ,playerwin)
